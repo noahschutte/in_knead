49f906b2fb8493afda89094f05842e9ecc8e1815
Noah Schutte <noahschutte@gmail.com>
2016-10-31 00:07:22 -0400

    add seed file and enable routes

--- db/migrate/20161031031335_create_users.rb
+++ db/migrate/20161031031335_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.integer :fb_userID, limit: 8
+      t.integer :fb_userID, limit: 8, null:false
       t.string :first_name, null: false
       t.string :signup_email, null: false
       t.string :current_email, null: false
--- db/migrate/20161031032341_create_thank_yous.rb
+++ db/migrate/20161031032341_create_thank_yous.rb
@@ -1,9 +1,6 @@
 class CreateThankYous < ActiveRecord::Migration[5.0]
   def change
     create_table :thank_yous do |t|
-      t.references :creator, null: false, index: true
-      t.string :first_name, null: false
-      t.string :video, null: false
 
       t.timestamps
     end
