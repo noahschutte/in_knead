502d2a95df2563b685ee554fec538b3fa32488d0
Noah Schutte <noahschutte@gmail.com>
2016-11-25 22:52:57 -0500

    add address to transcoded video content

--- app/models/thank_you.rb
+++ app/models/thank_you.rb
@@ -15,6 +15,8 @@ class ThankYou < ApplicationRecord
         pizzas: thank_you.pizzas,
         vendor: thank_you.vendor,
         video: get_url(thank_you.video),
+        compressed_video: get_compressed_url(thank_you.video),
+        thumbnail: get_thumbnail_url(thank_you.video),
         seconds: seconds,
         reports: thank_you.reports,
         created_at: thank_you.created_at,
@@ -33,6 +35,8 @@ class ThankYou < ApplicationRecord
         pizzas: thank_you.pizzas,
         vendor: thank_you.vendor,
         video: get_url(thank_you.video),
+        compressed_video: get_compressed_url(thank_you.video),
+        thumbnail: get_thumbnail_url(thank_you.video),
         seconds: seconds,
         reports: thank_you.reports,
         created_at: thank_you.created_at,
@@ -51,6 +55,8 @@ class ThankYou < ApplicationRecord
         pizzas: thank_you.pizzas,
         vendor: thank_you.vendor,
         video: get_url(thank_you.video),
+        compressed_video: get_compressed_url(thank_you.video),
+        thumbnail: get_thumbnail_url(thank_you.video),
         seconds: seconds,
         reports: thank_you.reports,
         created_at: thank_you.created_at,
@@ -63,6 +69,18 @@ class ThankYou < ApplicationRecord
     def self.get_url(video)
       @asset = S3_BUCKET.object("uploads/#{video}")
       @url = @asset.presigned_url(:get)
-      @url.sub!('in-knead.s3.amazonaws.com', "d32riymt5m6pak.cloudfront.net")
+      @url.sub('in-knead.s3.amazonaws.com', "d32riymt5m6pak.cloudfront.net")
+    end
+
+    def self.get_compressed_url(video)
+      @asset = S3_COMPRESSED_BUCKET.object("uploads/#{video}.mp4")
+      @url = @asset.presigned_url(:get)
+      # @url.sub!('in-knead.s3.amazonaws.com', "d32riymt5m6pak.cloudfront.net")
+    end
+
+    def self.get_thumbnail_url(video)
+      @asset = S3_THUMBNAIL_BUCKET.object("uploads/#{video}-00001.png")
+      @url = @asset.presigned_url(:get)
+      # @url.sub!('in-knead.s3.amazonaws.com', "d32riymt5m6pak.cloudfront.net")
     end
 end
