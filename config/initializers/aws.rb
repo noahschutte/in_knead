e66fc519c3cdb7483deaaccc746fc2b7c5926598
Noah Schutte <noahschutte@gmail.com>
2017-02-16 16:11:02 -0700

    rename environment variables

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,8 +3,10 @@ Aws.config.update({
   credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
 })
 
-# S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
-#
-# S3_COMPRESSED_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_COMPRESSED_BUCKET'])
-#
-# S3_THUMBNAIL_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_THUMBNAIL_BUCKET'])
+S3_REQUESTS = Aws::S3::Resource.new.bucket(ENV['S3_REQUESTS'])
+S3_REQUESTS_COMPRESSED = Aws::S3::Resource.new.bucket(ENV['S3_REQUESTS_COMPRESSED'])
+S3_REQUESTS_THUMBNAILS = Aws::S3::Resource.new.bucket(ENV['S3_REQUESTS_THUMBNAILS'])
+
+S3_THANKYOUS = Aws::S3::Resource.new.bucket(ENV['S3_THANKYOUS'])
+S3_THANKYOUS_COMPRESSED = Aws::S3::Resource.new.bucket(ENV['S3_THANKYOUS_COMPRESSED'])
+S3_THANKYOUS_THUMBNAILS = Aws::S3::Resource.new.bucket(ENV['S3_THANKYOUS_THUMBNAILS'])
