84c0eaca12d4c0ab5486630adb0812af9041a4db
Noah Schutte <noahschutte@lisi-personal.lan>
2020-03-10 14:53:31 -0600

    explicit 200 status

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -30,7 +30,7 @@ class RequestsController < ApplicationController
       if @request.save
         Request.update_video_key(@request, request_params[:videoKey])
         @signed_request = set_presigned_put_url(@request.video)
-        render :json => { signedRequest: @signed_request, videoKey: @request.video }
+        render :status => :ok, :json => { signedRequest: @signed_request, videoKey: @request.video }
       else
         render :status => 400, :json => { errorMessage: "Request could not be created." }
       end
